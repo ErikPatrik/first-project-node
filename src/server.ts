6ffeb4a45eea2c57f9f402e9af182757bad97e5b
Erik Patrik Pittaluga <erikpatrikpittaluga@gmail.com>
2022-02-01 23:21:22 -0300

    new changes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-	return response.json({
-		message: 'Olá',
-	});
+app.get("/", (request, response) => {
+    return response.json({
+        message: "Olá",
+    });
 });
 
 app.listen(3333, () => {
-	console.log('Server started! Port: 3333!');
+    console.log("Server started! Port: 3333!");
 });
